f26379522132c57b2100e9ffad6dd59feb4d2d4f
GB_Vanhelm <mpohilchuk@gmail.com>
2019-08-08 22:28:28 +0500

    массивы

--- oop.php
+++ oop.php
@@ -1,35 +1,22 @@
 <?php
 
-//учу ООП немного
-//Объявляю класс
-class ShopProduct{
-	//Объявляю свойства
-	public $title = "Стандартный товра";
-	public $name = "Имя";
-	public $firstName = "Фамилия";
-	public $price = 0;
-	//метод для быстрого создания объектов
-	function __construct($title, $name, $firstName, $price){
-		$this->title = $title;
-		$this->name = $name;
-		$this->firstName = $firstName;
-		$this->price = $price;
-	}
-	//Быстрый вывод
-	function getProducer(){
-		return "{$this->name} " . "{$this->firstName}";
-	}
+function include_template($name, array $data = [])
+{
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
 }
 
-class ShopProductWriter{
-	public function write(ShopProduct $ShopProduct){
-		$str = "$ShopProduct->title: " . $ShopProduct->getProducer() . " ($ShopProduct->price)";
-		return  $str;
-	}
-}
-
-$product = new ShopProduct("Собачье сердце", "Михаил", "Булгаков", 173);
-$writer = new ShopProductWriter();
-echo $writer->write($product);
 
 
